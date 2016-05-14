0bd6ecb05a6028fa1b0b38ddaa71c5db205ecde9
ilia-growmobile <ilia@growmobile.com>
2016-05-13 23:02:13 -0700

    fix authentication

--- app/controllers/twilio_api_controller.rb
+++ app/controllers/twilio_api_controller.rb
@@ -1,6 +1,6 @@
 class TwilioApiController < ApplicationController
+  skip_before_filter :verify_authenticity_token
   require 'twilio-ruby'
-
   def sms
     message = SmsRequestService.get_data(params)
     puts params.inspect
