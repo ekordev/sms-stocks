3720c10a1baa8b13010dc0daf3c25e83db7d89aa
ilia-growmobile <ilia@growmobile.com>
2016-05-13 20:35:25 -0700

    first ver

--- app/controllers/twilio_api_controller.rb
+++ app/controllers/twilio_api_controller.rb
@@ -1,23 +1,23 @@
 class TwilioApiController < ApplicationController
   require 'twilio-ruby'
   def sms
-    ticker = params['ticker']
+    ticker = params['Body']
     status = "OK"
     is_success, stock_price = StockService.get_price(ticker)
     if !is_success
       status = "ERROR"
       stock_price=0
     end
-    render :json =>
-      {
-        "status" => status,
-        "price": stock_price
-      },
-      status: 200
+    #render :json =>
+    #  {
+    #    "status" => status,
+    #    "price": stock_price
+    #  },
+    #  status: 200
 
-    #twiml = Twilio::TwiML::Response.new do |r|
-    #  r.Message "Price for stock symbol: #{ticker} is: #{stock_price}"
-    #end
-    #twiml.text
+    twiml = Twilio::TwiML::Response.new do |r|
+      r.Message "Price for stock symbol: #{ticker} is: #{stock_price}"
+    end
+    render xml: twiml.text
   end
 end
