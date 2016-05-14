5e1508e0c65bdef074e359eb87e09dce5683a7b2
ilia-growmobile <ilia@growmobile.com>
2016-05-13 20:57:42 -0700

    change to post

--- app/controllers/main.rb
+++ app/controllers/main.rb
@@ -13,7 +13,7 @@ auth_token = "a51d63d3a949926fa065ddcda003d672"   # Your Auth Token from www.twi
 #  end
 #puts message.sid
 
-#@client = Twilio::REST::Client.new account_sid, auth_token
-#@app = @client.account.applications.get("stocks")
-#@app.update(:sms_url => "http://demo.twilio.com/docs/sms.xml")
-#puts @app.voice_url
+@client = Twilio::REST::Client.new account_sid, auth_token
+@app = @client.account.applications.get("AP8a99cbc639f3e7ccadc8d18bcd43bff0")
+@app.update(:sms_url => "https://iliafain.herokuapp.com/twilio")
+puts @app.sms_url
